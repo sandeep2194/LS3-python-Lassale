d30f2847dfbd2fc55926f98da9bbe89708b4041d
omidpn <omidpnteaching@gmail.com>
2022-10-12 18:01:34 -0400

    lambda, functional programming, package and module

--- functions/functional_programming.py
+++ functions/functional_programming.py
@@ -1,4 +1,5 @@
 
+# predicate means, there is a true or false from a function or lambda 
 def positive(x):
     if x>0:
         return True
@@ -8,7 +9,9 @@ def positive1(x):
    return x>0
 
 li=[1,0,-1,-3,2]
+print('\n******filter built-in function is an itrable object , we change it to list ')
+print('\n******filter(function, itarable)')
 result=list(filter(positive1,li))
-result1=list(filter(lambda x: x>0,li))
-print(result)
-print(result1)
+print(f'Using of a function in filter as a predicate function named positive1:\n{result}')
+result_using_lambda=list(filter(lambda x: x>0,li))
+print(f'Using of a lambda in filter as a predicate function, there is no name for lambda:\n{result_using_lambda}')
